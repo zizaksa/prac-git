276af70bbfbb2516a054c15fc552e09da46ead2c
Gouyeon <gouyeonch@naver.com>
2020-11-04 17:21:21 +0900

    this is my second commit

---
+++ gugudan.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for(int i=0;i<9;i++)
+    {
+        printf(\n%d X %d = %d\n,N,i+1,N*(i+1));
+    }
+
+    return 0;
+}
